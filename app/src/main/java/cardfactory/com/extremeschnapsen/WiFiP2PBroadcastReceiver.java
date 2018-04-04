ea72f887e944a606e7ab708df315a0c0f04b96c0
Christian Roland Wuggenig <chwuggenig@edu.aau.at>
2018-04-04 21:18:30 +0200

    P2P-Implementation done, fine tuning still outstanding

--- app/src/main/java/cardfactory/com/extremeschnapsen/WiFiP2PBroadcastReceiver.java
+++ app/src/main/java/cardfactory/com/extremeschnapsen/WiFiP2PBroadcastReceiver.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.p2p.*;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by Christian on 03.04.2018.
@@ -12,34 +13,49 @@ import android.util.Log;
 
 public class WiFiP2PBroadcastReceiver extends BroadcastReceiver {
 
-    private WifiP2pManager mManager;
-    private WifiP2pManager.Channel mChannel;
-    private Search searchActivity;
-    WifiP2pManager.PeerListListener myPeerListListener;
+    //declare the necessary P2P-Objects
+    private WifiP2pManager p2pManager;
+    private WifiP2pManager.Channel p2pChannel;
+    WifiP2pManager.PeerListListener peerListListener;
 
-    public WiFiP2PBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel,
-                                    Search activity, WifiP2pManager.PeerListListener myPeerListListener) {
+    //declare activity-context to show toast-messages on the search-activity
+    private Search searchActivity;
+
+    public WiFiP2PBroadcastReceiver(WifiP2pManager manager,
+                                    WifiP2pManager.Channel channel,
+                                    Search activity,
+                                    WifiP2pManager.PeerListListener myPeerListListener) {
         super();
-        this.mManager = manager;
-        this.mChannel = channel;
+        this.p2pManager = manager;
+        this.p2pChannel = channel;
         this.searchActivity = activity;
-        this.myPeerListListener = myPeerListListener;
+        this.peerListListener = myPeerListListener;
     }
 
+    //is executed every time a system action with a defined filter in "intentFilters" is thrown
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
 
+        //decide which P2P-Action was thrown
         if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+
             int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+
+            //check if P2P is possible with the given hardware-settings (for example if P2P is not available for some reason, a toast message is shown)
             if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                Toast.makeText(searchActivity, R.string.msgWifiP2pSearching, Toast.LENGTH_SHORT).show();
                 Log.d("tag", "wifi p2p works!");
             } else {
-                // Wi-Fi P2P is not enabled
+                Toast.makeText(searchActivity, R.string.msgWifiP2pNotWorking, Toast.LENGTH_LONG).show();
             }
-        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
-            if (mManager != null) {
-                mManager.requestPeers(mChannel, myPeerListListener);
+        }
+        //this action is thrown when peers are available on the network
+        else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            if (p2pManager != null) {
+
+                //tell the manager to request the available peers
+                p2pManager.requestPeers(p2pChannel, peerListListener);
             }
 
         } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
