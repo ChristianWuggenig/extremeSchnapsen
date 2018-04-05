b20d6d4b7f22fca75879d98e12aebb054d15c3ab
NapeStar <mnapetsc@edu.aau.at>
2018-04-05 12:52:58 +0200

    nachdem bei mir auch die erste Activity drin ist-> bitte das verwenden Player.class -> Modell für Player PlayerDataSource.class -> DAO für Player DbHelper.class -> DB HilfsKlasse StartScreenActivity.class -> Startseite RegisiterActivity.class -> für RegistierSeite stirngs.xml -> hier Strings fürs layout speichern AndroidManifest.xml-> hab die StartScreenActivity ist jetz die MainActivity

--- app/src/main/java/cardfactory/com/extremeschnapsen/Player.java
+++ app/src/main/java/cardfactory/com/extremeschnapsen/Player.java
@@ -2,7 +2,41 @@ package cardfactory.com.extremeschnapsen;
 
 /**
  * Created by NapeStar on 03.04.18.
+ * Instance of this class (Player)can store data of corresponding SQLite-dataset and
+ * represent the dataset in the code.
+ * methods: getter und setter, constructor and toString()
  */
 
 public class Player {
+
+    private long id;
+    private String username;
+
+
+    public Player (long id, String username){
+        this.id = id;
+        this.username = username;
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String toString(){
+        String output = id + " " + username;
+     return output;
+    }
 }
---
+++ app/src/main/java/cardfactory/com/extremeschnapsen/PlayerDataSource.java
@@ -0,0 +1,98 @@
+package cardfactory.com.extremeschnapsen;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+import android.content.ContentValues;
+import android.database.Cursor;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Created by NapeStar on 04.04.18.
+ *This class is a Data Acces Object (DAO) and resposible for the CRUD management.
+ *It maintains the database connection and add, read and (delete) dataset in Table Player_List
+ *Converts database entries into Player objects
+ *methods: constructor, open(), close(), createPlayer(String username),
+ * cursorToPlayer(Cursor cursor), getAllPlayers(),
+ */
+
+public class PlayerDataSource {
+
+    //LOG_TAG for filtering in logcat
+    private static final String LOG_TAG = PlayerDataSource.class.getSimpleName();
+
+    private SQLiteDatabase database;
+    private DbHelper dbHelper;
+
+    private String[] columns = {
+            DbHelper.COLUMN_ID,
+            DbHelper.COLUMN_USERNAME
+    };
+
+    public PlayerDataSource(Context context) {
+        Log.d(LOG_TAG, "Unsere DataSource erzeugt jetzt den dbHelper.");
+        dbHelper = new DbHelper(context);
+    }
+
+    public void open() {
+        Log.d(LOG_TAG, "Eine Referenz auf die Datenbank wird jetzt angefragt.");
+        database = dbHelper.getWritableDatabase();
+        Log.d(LOG_TAG, "Datenbank-Referenz erhalten. Pfad zur Datenbank: " + database.getPath());
+    }
+
+    public void close() {
+        dbHelper.close();
+        Log.d(LOG_TAG, "Datenbank mit Hilfe des DbHelpers geschlossen.");
+    }
+
+    public Player createPlayer(String username) {
+        ContentValues values = new ContentValues();
+        values.put(DbHelper.COLUMN_USERNAME, username);
+
+        long insertId = database.insert(DbHelper.TABLE_PLAYER_LIST, null, values);
+
+        Cursor cursor = database.query(DbHelper.TABLE_PLAYER_LIST,
+                columns, DbHelper.COLUMN_ID + "=" + insertId,
+                null, null, null, null);
+
+        cursor.moveToFirst();
+        Player player = cursorToPlayer(cursor);
+        cursor.close();
+
+        return player;
+    }
+
+    private Player cursorToPlayer(Cursor cursor) {
+        int idIndex = cursor.getColumnIndex(DbHelper.COLUMN_ID);
+        int idUsername = cursor.getColumnIndex(DbHelper.COLUMN_USERNAME);
+
+        String username = cursor.getString(idUsername);
+        long id = cursor.getLong(idIndex);
+
+        Player player = new Player(id, username);
+
+        return player;
+    }
+
+    public List<Player> getAllPlayers() {
+        List<Player> playerList = new ArrayList<>();
+
+        Cursor cursor = database.query(DbHelper.TABLE_PLAYER_LIST,
+                columns, null, null, null, null, null);
+
+        cursor.moveToFirst();
+        Player player;
+
+        while(!cursor.isAfterLast()) {
+            player = cursorToPlayer(cursor);
+            playerList.add(player);
+            Log.d(LOG_TAG, "ID: " + player.getId() + ", Inhalt: " + player.toString());
+            cursor.moveToNext();
+        }
+
+        cursor.close();
+
+        return playerList;
+    }
+
+}
