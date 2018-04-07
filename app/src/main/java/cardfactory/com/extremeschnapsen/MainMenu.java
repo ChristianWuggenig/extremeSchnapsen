f7e20147e3dae169b8f9a49e2ca958441275bccf
Daniel Scheriau <daniel.scheriau@gmail.com>
2018-04-07 11:12:18 +0200

    menu

--- app/src/main/java/cardfactory/com/extremeschnapsen/MainMenu.java
+++ app/src/main/java/cardfactory/com/extremeschnapsen/MainMenu.java
@@ -2,6 +2,7 @@ package cardfactory.com.extremeschnapsen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -10,4 +11,7 @@ public class MainMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
     }
+
+
+
 }
