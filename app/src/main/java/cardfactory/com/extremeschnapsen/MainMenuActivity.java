f912287aa4a90086a441734c74423573c1c4e479
Christian Roland Wuggenig <chwuggenig@edu.aau.at>
2018-04-10 21:20:57 +0200

    Quality Assurance for issue_#4 done

--- app/src/main/java/cardfactory/com/extremeschnapsen/MainMenuActivity.java
+++ app/src/main/java/cardfactory/com/extremeschnapsen/MainMenuActivity.java
@@ -1,13 +1,25 @@
 package cardfactory.com.extremeschnapsen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+import android.view.Window;
 
 public class MainMenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void onClickBtnSearch(View view) {
+        Intent i = new Intent(this, SearchActivity.class);
+        this.startActivity(i);
     }
 }
