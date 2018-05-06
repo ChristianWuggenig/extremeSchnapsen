b4294a4bb8d465685d82032c2b211685e5a2c404
Daniel Scheriau <daniel.scheriau@gmail.com>
2018-05-06 12:12:10 +0200

    Datenbanktabelle RoundPoints inklusive DataSource wurde erstellt, Spiellogik am Ende der Round.java wurde eingefügt Methoden zum speichern und addieren der Punkte wurde erstellt
    Bitte sagts mir was ich wo ändern soll danke :)

---
+++ app/src/main/java/cardfactory/com/extremeschnapsen/RoundPointsDataSource.java
@@ -0,0 +1,115 @@
+package cardfactory.com.extremeschnapsen;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RoundPointsDataSource {
+
+    private static final String LOG_TAG = cardfactory.com.extremeschnapsen.RoundPointsDataSource.class.getSimpleName();
+
+    private SQLiteDatabase database;
+    private DbHelper dbHelper;
+
+
+    private String[] columns = {
+            DbHelper.COLUMN_ROUNDPOINTS_ID,
+            DbHelper.COLUMN_CURRENTROUNDPOINTS,
+            DbHelper.COLUMN_POINTSPLAYER1,
+            DbHelper.COLUMN_POINTSPLAYER2
+    };
+
+    public RoundPointsDataSource(Context context) {
+        Log.d(LOG_TAG, "Unsere PointsRoundDataSource erzeugt jetzt den dbHelper.");
+        dbHelper = DbHelper.getInstance(context);
+    }
+
+    public void open() {
+        Log.d(LOG_TAG, "Eine Referenz auf die Datenbank wird jetzt angefragt.");
+        database = dbHelper.getWritableDatabase();
+        Log.d(LOG_TAG, "Datenbank-Referenz erhalten. Pfad zur Datenbank: " + database.getPath());
+    }
+
+    public void close() {
+        dbHelper.close();
+        Log.d(LOG_TAG, "Datenbank mit Hilfe des DbHelpers geschlossen.");
+    }
+
+    public RoundPoints createRoundPoints(int RoundPointsID, int CurrentRoundPointsID, int PointsPlayer1, int PointsPlayer2) {
+        ContentValues values = new ContentValues();
+        values.put(DbHelper.COLUMN_ROUNDPOINTS_ID, RoundPointsID);
+        values.put(DbHelper.COLUMN_ROUNDPOINTS_ID, CurrentRoundPointsID);
+        values.put(DbHelper.COLUMN_POINTSPLAYER1, PointsPlayer1);
+        values.put(DbHelper.COLUMN_POINTSPLAYER2, PointsPlayer2);
+
+        long insertId = database.insert(DbHelper.TABLE_ROUNDPOINTS_LIST, null, values);
+
+        Cursor cursor = database.query(DbHelper.TABLE_ROUNDPOINTS_LIST,
+                columns, DbHelper.COLUMN_ROUNDPOINTS_ID + "=" + insertId,
+                null, null, null, null);
+
+        cursor.moveToFirst();
+        RoundPoints roundPoints = cursorToRoundpoints(cursor);
+        cursor.close();
+
+        return roundPoints;
+    }
+
+    private RoundPoints cursorToRoundpoints(Cursor cursor) {
+        int idRoundPointsID = cursor.getColumnIndex(DbHelper.COLUMN_ROUNDPOINTS_ID);
+        int idCurrentRoundPoints = cursor.getColumnIndex(DbHelper.COLUMN_CURRENTROUNDPOINTS);
+        int idPointsPlayer1 = cursor.getColumnIndex(DbHelper.COLUMN_POINTSPLAYER1);
+        int idPointsPlayer2 = cursor.getColumnIndex(DbHelper.COLUMN_POINTSPLAYER2);
+
+
+        int RoundPointsID = cursor.getInt(idRoundPointsID);
+        int CurrentRoundPoints = cursor.getInt(idCurrentRoundPoints);
+        int PointsPlayer1 = cursor.getInt(idPointsPlayer1);
+        int PointsPlayer2 = cursor.getInt(idPointsPlayer2);
+
+
+        long id = cursor.getLong(idRoundPointsID);
+
+        RoundPoints roundPoints;
+        roundPoints = new RoundPoints(RoundPointsID, CurrentRoundPoints, PointsPlayer1, PointsPlayer2);
+
+        return roundPoints;
+    }
+
+    public List<RoundPoints> getAllRoundPoints() {
+        List<RoundPoints> roundPointsList = new ArrayList<>();
+
+        Cursor cursor = database.query(DbHelper.TABLE_ROUNDPOINTS_LIST,
+                columns, null, null, null, null, null);
+
+        cursor.moveToFirst();
+        RoundPoints roundPoints;
+
+        while(!cursor.isAfterLast()) {
+            roundPoints = cursorToRoundpoints(cursor);
+            roundPointsList.add(roundPoints);
+            Log.d(LOG_TAG, "ID: " + roundPoints.getRoundpointsID() + ", Inhalt: " + roundPoints.toString());
+            cursor.moveToNext();
+        }
+
+        cursor.close();
+        return roundPointsList;
+    }
+
+    public void saveRoundPoints(RoundPoints pointsToSave){
+        ContentValues values = new ContentValues();
+        values.put(DbHelper.COLUMN_CURRENTROUNDPOINTS, pointsToSave.getCurrentroundpoints());
+        values.put(DbHelper.COLUMN_ROUNDPOINTS_ID, pointsToSave.getRoundpointsID());
+        values.put(DbHelper.COLUMN_POINTSPLAYER1, pointsToSave.getPointsplayer1());
+        values.put(DbHelper.COLUMN_POINTSPLAYER2, pointsToSave.getPointsplayer2());
+        database.insert(DbHelper.TABLE_ROUNDPOINTS_LIST, null, values);
+    }
+
+}
+
+
